98fa6d885019dc1a374c470686a74553f9a45d94
Nguyen, GIANG <yakiro.nvg@gmail.com>
2021-05-07 18:41:10 +0700

    delete task on finalizer

--- test/helloa1.cpp
+++ test/helloa1.cpp
@@ -5,9 +5,11 @@
 
 #include "helloa1.cam.h"
 
-static void helloa1_end(struct cam_s *cam, cam_tid_t tid, void *)
+static void helloa1_end(struct cam_s *cam, cam_tid_t tid, void *ktx)
 {
         printf("<HELLOA1 - END>\n");
+        auto task = *(cam_tid_t*)ktx;
+        cam_task_delete(cam, task);
 }
 
 TEST_CASE("helloa1")
@@ -22,11 +24,11 @@ TEST_CASE("helloa1")
         auto helloa1 = cam_resolve(cam, "HELLOA1");
         REQUIRE(cam_is_alive_program(cam, helloa1));
 
-        auto task = cam_task_new(cam, &ec, helloa1, nullptr, 0, helloa1_end, nullptr);
+        cam_tid_t task;
+        task = cam_task_new(cam, &ec, helloa1, nullptr, 0, helloa1_end, &task);
         REQUIRE(ec == CEC_SUCCESS);
         REQUIRE(cam_is_alive_task(cam, task));
         cam_resume(cam, task);
 
-        cam_task_delete(cam, task);
         cam_delete(cam);
 }
