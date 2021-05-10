7b9a65a9d1f5a7c5bc198a01ca6dfd411ffef821
Nguyen, Giang <yakiro.nvg@gmail.com>
2021-05-10 19:44:20 +0700

    add gnucobol integration example

--- examples/asmmixa1_gnucobol/asmmixa1.cpp
+++ examples/asmmixa1_gnucobol/asmmixa1.cpp
@@ -1,30 +1,31 @@
-/* Copyright (c) 2021 FPT Software - All Rights Reserved. Proprietary.
- * Unauthorized copying of this file, via any medium is strictly prohibited. */
-#include <catch.hpp>
 #include <cam.h>
+#include <libcob.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #include "ASMMIXA1.cam.h"
 #include "ASMMIXA6.cam.h"
 
-TEST_CASE("asmmix")
+extern "C" int ASMMIXA1()
 {
         cam_error_t ec;
         auto cam = cam_new(&ec);
-        REQUIRE(cam != nullptr);
-        REQUIRE(ec == CEC_SUCCESS);
+        CAM_ASSERT(cam != nullptr);
+        CAM_ASSERT(ec == CEC_SUCCESS);
 
         ec = cam_load_chunk(cam, ASMMIXA1_cam, sizeof(ASMMIXA1_cam));
-        REQUIRE(ec == CEC_SUCCESS);
+        CAM_ASSERT(ec == CEC_SUCCESS);
         auto asmmixa1 = cam_resolve(cam, "ASMMIXA1");
 
         ec = cam_load_chunk(cam, ASMMIXA6_cam, sizeof(ASMMIXA6_cam));
-        REQUIRE(ec == CEC_SUCCESS);
+        CAM_ASSERT(ec == CEC_SUCCESS);
 
         cam_tid_t task;
         task = cam_task_new(cam, &ec, asmmixa1, nullptr, 0, cam_nop_k, nullptr);
-        REQUIRE(ec == CEC_SUCCESS);
+        CAM_ASSERT(ec == CEC_SUCCESS);
         cam_resume(cam, task);
 
         cam_task_delete(cam, task);
         cam_delete(cam);
+        return 0;
 }
